ce3bf066e3b6592f1f5c394c86e8770379fb594e
Artem Harkusha <Atem.Harkusha@rotary.org>
2021-03-01 14:30:25 +0200

    add trigger

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -46,6 +48,10 @@ object UnitTests: BuildType({
             scriptContent = "echo 'executing unit tests'"
         }
     }
+
+    triggers {
+        vcs {}
+    }
 })
 
 object StaticSite: BuildType({
@@ -65,4 +71,3 @@ object FunctionApp: BuildType({
         }
     }
 })
-
